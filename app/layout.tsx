8e6b136358865a9decced820eb0081ae114b69e9
Sanjay Ganapathi <gana.sanjay12@gmail.com>
2024-12-27 14:40:34 +0530

    chat landing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 
 const geistSans = localFont({
@@ -12,6 +13,8 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
---
+++ components/ui/ai-input.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Send } from "lucide-react";
+import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { useAutoResizeTextarea } from "@/hooks/use-auto-resize-textarea";
+
+const MIN_HEIGHT = 132;
+const MAX_HEIGHT = 264;
+
+export default function AIInput() {
+    const [value, setValue] = useState("");
+    const { textareaRef, adjustHeight } = useAutoResizeTextarea({
+        minHeight: MIN_HEIGHT,
+        maxHeight: MAX_HEIGHT,
+    });
+
+    const handleSubmit = () => {
+        setValue("");
+        adjustHeight(true);
+    };
+
+    return (
+        <div className="w-full py-4">
+            <div className="relative max-w-xl w-full mx-auto">
+                <div className="relative flex flex-col">
+                    <div
+                        className="overflow-y-auto"
+                        style={{ maxHeight: `${MAX_HEIGHT}px` }}
+                    >
+                        <Textarea
+
+                            value={value}
+                            placeholder="Search the web..."
+                            className="w-full rounded-xl px-4 py-3 bg-white/5 border-none text-white  placeholder:text-white/70 resize-none focus-visible:ring-0 leading-[1.2]"
+                            ref={textareaRef}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter" && !e.shiftKey) {
+                                    e.preventDefault();
+                                    handleSubmit();
+                                }
+                            }}
+                            onChange={(e) => {
+                                setValue(e.target.value);
+                                adjustHeight();
+                            }}
+                        />
+                    </div>
+
+
+                    <div className="absolute right-3 bottom-3">
+                        <button
+                            type="button"
+                            onClick={handleSubmit}
+                            className={cn(
+                                "rounded-lg p-2 transition-colors",
+                                value
+                                    ? "bg-sky-500/15 text-sky-500"
+                                    : " bg-white/5 text-white/40 hover:text-white"
+                            )}
+                        >
+                            <Send className="w-4 h-4" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
